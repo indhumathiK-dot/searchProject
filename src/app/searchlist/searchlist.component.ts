a0781ed886760070ab88a89e39d81d1989393a63
Indhu <indhumathi@softsuave.com>
2021-03-02 17:37:10 +0530

    Initial commit

---
+++ src/app/searchlist/searchlist.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchlist',
+  templateUrl: './searchlist.component.html',
+  styleUrls: ['./searchlist.component.scss']
+})
+export class SearchlistComponent implements OnInit {
+  heroes = [];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.heroes = [
+
+    ];
+  }
+
+}
